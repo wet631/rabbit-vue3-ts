53320a31a0dbc6da9ebeb1f2d4b939497faf1e84
wet631 <2467530813@qq.com>
2023-09-19 22:59:15 +0800

    提交公共样式

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,11 @@
-<script setup></script>
+<script setup lang="ts">
+import request from '@/utils/request'
+const test = async () => {
+  const res = await request.get('/home/index')
+  console.log(res)
+}
+test()
+</script>
 <template>
   <div>111</div>
 </template>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,27 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
+// 标准用法 node环境 需要使用require
+// 原因在于vite中没有不支持require()
+// const path = require('path')
+import path from 'path' 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  },
+  // 配置css的全局注入
+  css: {
+    // 预加载
+    preprocessorOptions: {
+      less: {
+        additionalData: `
+          @import "@/assets/styles/variables.less";
+          @import "@/assets/styles/mixins.less";
+        `
+      }
+    }
+  }
 })
